f6c8fd71f1f355805fd2e5b53305a010979db465
ArkinSolomon <arkin@arkinsolomon.net>
2022-11-25 13:44:25 -0600

    Work on returning packages

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -12,6 +12,45 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied limitations under the License.
  */
+
+/**
+ * The data given from the server for a package version.
+ * 
+ * @typedef {Object} VersionData
+ * @property {string} version The version string.
+ * @property {string} hash The hash of the data in the package.
+ * @property {boolean} approved True if the package is approved.
+ * @property {boolean} published True if the package is published.
+ * @property {string} loc The URL from which to download the package.
+ */
+type VersionData = {
+  version: string;
+  hash: string;
+  approved: boolean;
+  publishd: boolean;
+  loc: string;
+};
+
+/**
+ * The data given from the server for a single package.
+ * 
+ * @typedef {Object} PackageData
+ * @property {string} packageId The id of the package.
+ * @property {string} packageName The name of the package.
+ * @property {string} packageDescription The description of the package.
+ * @property {number} installs The number of installations the package has.
+ * @property {string} packageType The type of the package.
+ * @property {VersionData[]} versions The versions of the package.
+ */
+type PackageData = {
+  packageId: string;
+  packageName: string;
+  packageDescription: string;
+  installs: number;
+  packageType: string;
+  versions: VersionData[];
+};
+
 import { Router } from 'express';
 import { AuthTokenPayload, validateName } from './auth.js';
 import mysql from 'mysql2';
@@ -32,8 +71,7 @@ route.post('/changename', (req, res) => {
 
   const checkName = newName.trim().toLowerCase();
   if (user.name.trim().toLowerCase() === checkName ||
-    !validateName(checkName)
-  )
+    !validateName(checkName))
     return res.sendStatus(400);
 
   const getQuery = mysql.format('SELECT lastChange, authorEmail FROM authors WHERE authorId = ? AND session = ?;', [user.id, user.session]);
@@ -56,7 +94,6 @@ route.post('/changename', (req, res) => {
 
     const session = await nanoid(16);
 
-
     // TODO test update of packages table
     const updateQuery1 = mysql.format('UPDATE authors SET authorName = ?, checkName = ?, session = ?, lastChange = ? WHERE authorId = ?;', [newName.trim(), checkName, session, new Date(), user.id]);
     const updateQuery2 = mysql.format('UPDATE packages SET authorName = ? WHERE authorId = ?;', [newName.trim(), user.id]);
@@ -70,6 +107,42 @@ route.post('/changename', (req, res) => {
       return res.sendStatus(500);
     }
   });
+});
+
+route.post('/packages', async (req, res) => {
+  const { id } = req.user as AuthTokenPayload;
+  if (!id)
+    return res.sendStatus(401);
+
+  const packagesQuery = mysql.format('SELECT packageId, packageName, description, packageType, installs FROM packages WHERE authorId = ?;', id);
+  const versionsQuery = mysql.format('SELECT packageId, version, HEX(hash), approved, published, loc FROM versions WHERE authorId = ?;', id);
+  try {
+    const data: PackageData[] = [];
+    const packages: Omit<PackageData, 'versions'>[] = await query(packagesQuery);
+    const versions: (VersionData & { packageId?: string; })[] = await query(versionsQuery);
+
+    for (const pkg of packages) {
+      const pkgData: PackageData = {
+        ...pkg,
+        versions: []
+      };
+
+      let v = versions.findIndex(v => v.packageId === pkg.packageId);
+      while (v > -1) {
+        const version = versions.splice(v, 1)[0];
+        delete version.packageId;
+        pkgData.versions.push(version);
+        v = versions.findIndex(v => v.packageId === pkg.packageId);
+      }
+
+      data.push(pkgData);
+    }
+
+    res.json(data);
+  } catch (e) {
+    console.error(e);
+    return res.sendStatus(500);
+  }
 });
 
 export default route;
